11a0493ceea580c8bdbc8d26c668f8ad3a08e1c6
gnsgmlckdgk@naver.com <dohauzi@gmail.com>
2023-07-01 05:34:59 +0900

    Hello Webpack

---
+++ 2. 끝말잇기/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');  // NodeJS를 설치하면 path는 설치되어있다.
+
+module.exports = {
+    name: 'word-relay-setting',
+
+    mode: 'development',    // 실서비스 : production
+    devtool: 'eval',                // eval : 빠르게
+
+    resolve: {
+        // entry에서 입력 app 에 파일넣을때 검색할 확장자 설정, 이 설정이 없으면 app ['./client.jsx'] 와 같이 확장자 넣어서 추가 필요함
+        extensions: ['.js', '.jsx']
+    },
+    entry: {    // 입력
+        // client.jsx에서 WordRelay.jsx를 불러오기 때문에 WordRelay.jsx는 추가 할 필요 없다.
+        app: ['./client'],
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+            }
+        }],
+    },
+
+    output: {   // 출력
+        path: path.join(__dirname, 'dist'), // __dirname은 현재 파일의 절대경로를 동적으로 가져옴, 현재파일경로/dist
+        filename: 'app.js'
+    },
+}

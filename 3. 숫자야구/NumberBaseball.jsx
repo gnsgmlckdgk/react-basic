346fd841ba7a859f4fcb9c6fc49cb64576e54066
gnsgmlckdgk@naver.com <dohauzi@gmail.com>
2023-09-03 16:48:27 +0900

    숫자야구

--- 3. 숫자야구/NumberBaseball.jsx
+++ 3. 숫자야구/NumberBaseball.jsx
@@ -7,7 +7,13 @@ const Try = require('./Try.jsx');
 
 // 숫자 4개를 겹치지 않게 랜덤하게 만드는 함수
 function getNumbers() {
-
+    const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 
 class NumberBaseball extends Component {
@@ -15,16 +21,58 @@ class NumberBaseball extends Component {
     state = {
         result: '',
         value: '',
-        answer: getNumbers(),
-        tries: []
+        answer: getNumbers(),   // ex: [1, 3, 5, 7]
+        tries: [],
     }
 
     onSubmitForm = (e) => {
-
+        e.preventDefault();
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({
+                result: '홈런!',
+                tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }],
+                // 리엑트에서는 push 쓰면 안됨(리엑트에서 변경됬는지 인식을 못함), ...array 를 해주면(이전거 복사) push처럼 됨
+            });
+            alert('게임을 다시 시작합니다!');
+            this.setState({
+                value: '',
+                answer: getNumbers(),
+                tries: [],
+            });
+        } else {    // 답이 틀렸으면
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (this.state.tries.length >= 9) {
+                this.setState({
+                    result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')} 였습니다!`
+                });
+                alert('게임을 다시 시작합니다!');
+                this.setState({
+                    value: '',
+                    answer: getNumbers(),
+                    tries: [],
+                });
+            } else {
+                for (let i = 0; i < 4; i++) {
+                    if (answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (this.state.answer.includes(answerArray[i])) {
+                        ball += 1;
+                    }
+                    this.setState({
+                        tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼` }]
+                    });
+                }
+            }
+        }
     }
 
     onChangeInput = (e) => {
-
+        console.log(this.state.answer);
+        this.setState({
+            value: e.target.value,
+        });
     }
 
     render() {
@@ -36,18 +84,10 @@ class NumberBaseball extends Component {
                 </form>
                 <div>시도 : {this.state.tries.length}</div>
                 <ul>
-                    {[
-                        { fruit: '사과', taste: '맛있다' },
-                        { fruit: '바나나', taste: '맛없다' },
-                        { fruit: '포도', taste: '시다' },
-                        { fruit: '귤', taste: '시다' },
-                        { fruit: '감', taste: '시다' },
-                        { fruit: '배', taste: '시다' },
-                        { fruit: '밤', taste: '시다' },
-                    ].map((v, i) => {
+                    {this.state.tries.map((v, i) => {
                         return (
-                            <Try key={v.fruit + v.taste} value={v} index={i} />
-                        );
+                            <Try key={`${i + 1}차 시도 : `} tryInfo={v} />
+                        )
                     })}
                 </ul>
             </>
--- 3. 숫자야구/Try.jsx
+++ 3. 숫자야구/Try.jsx
@@ -5,12 +5,8 @@ class Try extends Component {
     render() {
         return (
             <li>
-                <b>{this.props.index + 1}. {this.props.value.fruit}</b> = {this.props.value.taste}
-                <div>컨텐츠1</div>
-                <div>컨텐츠2</div>
-                <div>컨텐츠3</div>
-                <div>컨텐츠4</div>
-                <div>컨텐츠3</div>
+                <div>{this.props.tryInfo.try}</div>
+                <div>{this.props.tryInfo.result}</div>
             </li>
 
         )
